b33e2c125971a6cbfd11272588f80a2d6f33ea63
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-12-30 13:40:37 -0500

    Update PersonalNews.jsx

--- src/app/PersonalNews.jsx
+++ src/app/PersonalNews.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
 import useFetch from "../hooks/useFetch";
 import FeedItem from "../components/FeedItem";
 import Spinner from "../components/Spinner";
